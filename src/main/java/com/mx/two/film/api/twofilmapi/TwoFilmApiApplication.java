d177d2c6adc59b9eda789ad52c018770cc4b3f91
jatoni <jat_antonio_torres.juan@outlook.com>
2023-08-15 18:35:01 -0600

    Se empieza el proyecto de la api para la producturo audiovisual two film

---
+++ src/main/java/com/mx/two/film/api/twofilmapi/TwoFilmApiApplication.java
@@ -0,0 +1,13 @@
+package com.mx.two.film.api.twofilmapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwoFilmApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwoFilmApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mx/two/film/api/twofilmapi/TwoFilmApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mx.two.film.api.twofilmapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwoFilmApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
